bef9abad8d28fffc183809ce6eebec6c940f2ee5
Ethan <ebonsignori@gmail.com>
2019-03-09 17:06:45 -0500

    basic js and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+
+let char1Get = $( '#char1' );
+let char2Get = $( '#char2' );
+let char3Get = $( '#char3' );
+let char4Get = $( '#char4' );
+
+  char1 = {
+    hp: 100,
+    atk: 10,
+    catk: 5,
+  };
+
+  char2 = {
+    hp: 100,
+    atk: 10,
+    catk: 5,
+  };
+
+  char3 = {
+    hp: 100,
+    atk: 10,
+    catk: 5,
+  };
+
+  char4 = {
+    hp: 100,
+    atk: 10,
+    catk: 5,
+  };
+
+});
