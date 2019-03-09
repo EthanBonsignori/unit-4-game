ed0678b0b2e9c8ca9b9dd13e46a8dfca97f50908
Ethan <ebonsignori@gmail.com>
2019-03-09 18:04:35 -0500

    fixed issues

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,8 @@
 $( document ).ready(function() {
 
-let char1Get = $( '#char1' );
-let char2Get = $( '#char2' );
-let char3Get = $( '#char3' );
-let char4Get = $( '#char4' );
+  // Declare variables
+  let playerChar = null;
+
 
   char1 = {
     hp: 100,
@@ -28,5 +27,31 @@ let char4Get = $( '#char4' );
     atk: 10,
     catk: 5,
   };
+
+  // Get html elements
+  let char1Get = $( '#char1' );
+  let char2Get = $( '#char2' );
+  let char3Get = $( '#char3' );
+  let char4Get = $( '#char4' );
+
+  let getChar = $( '.characters' );
+
+  // Select a character
+  getChar.on("click", function() {
+    let charVal = $(this.getChar).val()
+    console.log(charVal)
+    if ($(this).val() == '1') {
+      playerChar = getChar;
+      console.log("!!!" + playerChar)
+    }
+  }); // End character select
+
+  // Select an enemy
+
+  // Attack & Defend
+
+  // Win & Lose
+
+  // Reset
 
 });
