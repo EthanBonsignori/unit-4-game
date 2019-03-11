7fe902246b8aadfbd261f8d6140c9aa715ee5bae
Ethan <ebonsignori@gmail.com>
2019-03-11 17:24:30 -0400

    selection of player, selection of enemy to fight working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,118 +1,126 @@
-$( document ).ready(function() {
+$( document ).ready( function() {
 
   // Declare variables
   let playerChar = null;
   let playerName = '';
+  let fighterName = '';
   let charId = '';
+  let fighterId = '';
   let hasId = false;
+  let isFighting = false;
+  let enemies = [];
+
+  let consoleLog = 0;
+
 
   chars = [
     char1 = {
+      name: 'Pooh',
       hp: 100,
       atk: 10,
       catk: 5,
-      player: false,
-      enemy: false,
+      isPlayer: false,
+      isEnemy: false,
       charSelected: false,
       charGet: $( '#char1' ),
+      charId: 'char1',
     },
 
     char2 = {
+      name: 'Piglet',
       hp: 100,
       atk: 10,
       catk: 5,
-      player: false,
-      enemy: false,
+      isPlayer: false,
+      isEnemy: false,
       charSelected: false,
       charGet: $( '#char2' ),
+      charId: 'char2',
     },
 
     char3 = {
+      name: 'Tigger',
       hp: 100,
       atk: 10,
       catk: 5,
-      player: false,
-      enemy: false,
+      isPlayer: false,
+      isEnemy: false,
       charSelected: false,
-      charGet: $( '#char3' )
+      charGet: $( '#char3' ),
+      charId: 'char3',
     },
 
     char4 = {
+      name: 'Owl',
       hp: 100,
       atk: 10,
       catk: 5,
-      player: false,
-      enemy: false,
+      isPlayer: false,
+      isEnemy: false,
       charSelected: false,
-      charGet: $( '#char4' )
+      charGet: $( '#char4' ),
+      charId: 'char4',
     },
   ];
 
   // Get html elements
   let charsGet = $( '.characters' );
-  let enemyDiv = $( '.hidden' );
+
+  let enemyDiv = $( '.enemies' );
   let selectText = $( '#your-char' )
+  let defenderDiv = $( '.defender' )
+  let attackButton = $( '.attack-button' )
 
   // Select a character
-  charsGet.on("click", function() {
-    if (!hasId) { 
+  charsGet.on( "click", function() {
+    if ( !hasId && !playerChar ) { 
       charId = this.id;
-    }
-    if (!playerChar) {
-      if (charId == 'char1') {
-        playerChar = char1;
-        playerName = "Name1";
-        char1.player = true;
-      }
-      if (charId == 'char2') {
-        playerChar = char2;
-        playerName = "Name2";
-        char2.player = true;
-      }
-      if (charId == 'char3') {
-        playerChar = char3;
-        playerName = "Name3";
-        char3.player = true;
-      }
-      if (charId == 'char4') {
-        playerChar = char4;
-        playerName = "Name4";
-        char4.player = true;
-      }
-      findEnemy();
+      charsGet.each( function( i ) { 
+        if ( chars[i].charId === charId ) {
+          playerChar = chars[i];
+          playerName = chars[i].name;
+          chars[i].isPlayer = true;
+          chars[i].charGet.addClass( 'player' )
+        } else {
+          chars[i].isEnemy = true;
+          chars[i].charGet.addClass( 'enemy' )
+        }
+      })
       moveEnemy();
       hasId = true;
     }
+
+    // Select an enemy to Fight
+    if ( $( this ).hasClass( 'enemy' ) ) {
+      if ( !isFighting )
+        fighterId = this.id
+        isFighting = true;
+        charsGet.each( function( i ) {
+          if ( chars[i].charId === fighterId ) {
+            fighterName = chars[i].name
+            defenderDiv.append( this )
+            // Animations
+            defenderDiv.show( 'slow' )
+            attackButton.show( 'slow' )
+          }
+        })
+    }
   });
 
-  // Finds player and enemies
-  findEnemy = () => {
-    for (let i = 0; i < chars.length; i++) {
-      if (chars[i].player) {
-        chars[i].charSelected = true;
-      } 
-      if (!chars[i].charSelected) {
-        chars[i].enemy = true;
-      }
-    }
-  
-  // Pushes enemies to the enemy div and changes player border
+  // Pushes enemies to the enemy div
   moveEnemy = () => {
-    for (let i = 0; i < chars.length; i++)
-      if (chars[i].charSelected){
-        chars[i].charGet.addClass( 'player' )
-      } else {
-        chars[i].charGet.addClass( 'enemy' )
-        chars[i].charGet.appendTo( enemyDiv )
-      }
+    $( '.enemy ').each( function( i ) {
+      enemies.push( chars[i].name );
+      enemyDiv.append( this )
+      // Animations
       enemyDiv.show( 'slow' )
-      selectText.fadeOut(350, function() {
-        selectText.text( 'Your Character' ).fadeIn(350);
+      selectText.fadeOut( 350, function() {
+        selectText.text( 'Your Character' ).fadeIn( 350 );
       })
+    })
   }
-    
-  }
-  // Select an enemy
+
+  // Select an enemy to battle
 
   // Attack & Defend
 
@@ -121,10 +129,17 @@ $( document ).ready(function() {
   // Reset
 
   // Console log on ` backquote `
-  $( document ).keyup(function(event) {
-    if (event.keyCode == 192) {
-      console.log("Selected Character: " + playerName)
-      console.log("Character ID: " + charId);
+  $( document ).keyup( function( event ) {
+    if ( event.keyCode == 192 ) {
+      consoleLog++;
+      let enemyList = enemies.join( ', ' );
+      console.log( '======== Begin Log ' + consoleLog + ' ========' )
+      console.log( 'Player Name: ' + playerName );
+      console.log( 'Character ID: ' + charId );
+      console.log( 'Enemies: ' + enemyList );
+      console.log( 'Fighter name: ' + fighterName );
+      console.log( 'Fighter ID: ' + fighterId );
+      console.log( '======== End Log ' + consoleLog + ' ========' )
     }
   });
 
