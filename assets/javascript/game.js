489305242df70156ee46ea47429dac01cdd3f53f
Ethan <ebonsignori@gmail.com>
2019-03-11 22:04:58 -0400

    working on reset button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,80 @@
 $( document ).ready( function() {
 
-  // Declare variables
-  let playerChar = null;
-  let playerId = '';
+// Declare variables
+let playerChar, playerId, fighterChar, fighterId, hasId, isFighting, enemies, consoleLog, chars, gameLost, gameWon;
 
-  let fighterChar = null;
-  let fighterId = '';
+  //initialize = () => {
+    
+    playerChar = null;
+    playerId = '';
 
-  let hasId = false;
-  let isFighting = false;
+    fighterChar = null;
+    fighterId = '';
 
-  let enemies = [];
+    hasId = false;
+    isFighting = false;
+    gameLost = false;
+    gameWon = false;
 
-  let consoleLog = 0;
+    enemies = [];
+
+    consoleLog = 0;
 
 
-  chars = [
-    char1 = {
-      name: 'Pooh',
-      charId: 'char1',
-      hp: 100,
-      atk: 7,
-      baseAtk: 10,
-      counterAtk: 5,
-      isPlayer: false,
-      isEnemy: false,
-      charGet: $( '#char1' ),
-      charHpGet: $( '#char1-hp' ),
-    },
+    chars = [
+      char1 = {
+        name: 'Pooh',
+        charId: 'char1',
+        hp: 100,
+        atk: 7,
+        baseAtk: 10,
+        counterAtk: 5,
+        isPlayer: false,
+        isEnemy: false,
+        charGet: $( '#char1' ),
+        charHpGet: $( '#char1-hp' ),
+      },
 
-    char2 = {
-      name: 'Piglet',
-      charId: 'char2',
-      hp: 100,
-      atk: 10,
-      baseAtk: 10,
-      counterAtk: 5,
-      isPlayer: false,
-      isEnemy: false,
-      charGet: $( '#char2' ),
-      charHpGet: $( '#char2-hp' ),
-    },
+      char2 = {
+        name: 'Piglet',
+        charId: 'char2',
+        hp: 100,
+        atk: 100,
+        baseAtk: 10,
+        counterAtk: 1000,
+        isPlayer: false,
+        isEnemy: false,
+        charGet: $( '#char2' ),
+        charHpGet: $( '#char2-hp' ),
+      },
 
-    char3 = {
-      name: 'Tigger',
-      charId: 'char3',
-      hp: 100,
-      atk: 10,
-      baseAtk: 10,
-      counterAtk: 5,
-      isPlayer: false,
-      isEnemy: false,
-      charGet: $( '#char3' ),
-      charHpGet: $( '#char3-hp' ),
-    },
+      char3 = {
+        name: 'Tigger',
+        charId: 'char3',
+        hp: 100,
+        atk: 10,
+        baseAtk: 10,
+        counterAtk: 5,
+        isPlayer: false,
+        isEnemy: false,
+        charGet: $( '#char3' ),
+        charHpGet: $( '#char3-hp' ),
+      },
 
-    char4 = {
-      name: 'Owl',
-      charId: 'char4',
-      hp: 100,
-      atk: 10,
-      baseAtk: 10,
-      counterAtk: 5,
-      isPlayer: false,
-      isEnemy: false,
-      charGet: $( '#char4' ),
-      charHpGet: $( '#char4-hp' ),
-    },
-  ];
+      char4 = {
+        name: 'Owl',
+        charId: 'char4',
+        hp: 100,
+        atk: 10,
+        baseAtk: 10,
+        counterAtk: 5,
+        isPlayer: false,
+        isEnemy: false,
+        charGet: $( '#char4' ),
+        charHpGet: $( '#char4-hp' ),
+      },
+    ];
+  //}; // End Initialize
 
   // Get html elements
   let charsGet = $( '.characters' );
@@ -77,10 +84,11 @@ $( document ).ready( function() {
   let defenderDiv = $( '.defender' )
   let attackButton = $( '.attack-button' )
   let attack = $( '#attack' )
+  let resetButton = $( '#reset-button' )
 
   // Select a character
   charsGet.on( "click", function() {
-    if ( !hasId && !playerChar ) { 
+    if ( !hasId && !playerChar && !gameLost) { 
       playerId = this.id;
       charsGet.each( function( i ) { 
         if ( chars[i].charId === playerId ) {
@@ -92,7 +100,7 @@ $( document ).ready( function() {
           chars[i].isEnemy = true;
           chars[i].charGet.addClass( 'enemy' )
         }
-      })
+      } )
       moveEnemy();
       hasId = true;
     };
@@ -113,10 +121,10 @@ $( document ).ready( function() {
           }
           enemiesText.fadeOut( 350, function() {
             enemiesText.text( 'Fighting...' ).fadeIn( 350 );
-          })
-        })
+          } )
+        } )
     }
-  });
+  } );
 
   // Pushes enemies to the enemy div
   moveEnemy = () => {
@@ -127,50 +135,76 @@ $( document ).ready( function() {
       enemyDiv.show( 'slow' );
       selectText.fadeOut( 350, function() {
         selectText.text( 'Your Character' ).fadeIn( 350 );
-      })
-    })
+      } )
+    } )
   };
 
   // Attack & Defend
   attack.on( 'click', function() {
-      playerChar.hp -= fighterChar.counterAtk;        // Player loses hp equal to fighter's counter attack
-      fighterChar.hp -= playerChar.atk;               // Fighter loses hp equal to player's attack
-      // Update HP values on screen
-      playerChar.charHpGet.text( playerChar.hp );     
-      fighterChar.charHpGet.text( fighterChar.hp );
-      // Check if player will fall to or below 0 hp on next attack
-    if ( playerChar.hp <= fighterChar.counterAtk ) {
-      playerChar.hp -= fighterChar.counterAtk;
-      loseGame();
-      // Check if fighter will fall to or below 0 hp on next attack
-    } else if ( fighterChar.hp <= playerChar.atk ) {
-      fighterChar.hp -= playerChar.atk;
-      winFight();
+    if ( fighterChar && playerChar.hp > 0 ) {
+        playerChar.hp -= fighterChar.counterAtk;        // Player loses hp equal to fighter's counter attack
+        fighterChar.hp -= playerChar.atk;               // Fighter loses hp equal to player's attack
+        // Update HP values on screen
+        playerChar.charHpGet.text( playerChar.hp );     
+        fighterChar.charHpGet.text( fighterChar.hp );
+        // Check if player will fall to or below 0 hp on next attack
+      if ( playerChar.hp <= fighterChar.counterAtk ) {
+        playerChar.hp -= fighterChar.counterAtk;
+        loseGame();
+        // Check if fighter will fall to or below 0 hp on next attack
+      } else if ( fighterChar.hp <= playerChar.atk ) {
+        fighterChar.hp -= playerChar.atk;
+        winFight();
+      }
+      playerChar.atk += playerChar.baseAtk;           // Increment player's attack by base attack
     }
-    playerChar.atk += playerChar.baseAtk;           // Increment player's attackk by base attack
-  });
+  } );
 
   // Win & Lose
   winFight = () => {
-    console.log("you won this fight")
-    fighterChar.charGet.remove();
+    console.log( 'you won this fight' )
+    fighterChar.charGet.hide();
     enemies.splice( enemies.indexOf( fighterChar.name ), 1 );
     if ( enemies.length === 0 ) {
       winGame();
     } else {
-    fighterChar = null;
-    isFighting = false;
+      fighterChar = null;
+      isFighting = false;
+      // Animations
+      defenderDiv.hide();
+      attackButton.hide();
+      enemiesText.fadeOut( 350, function() {
+        enemiesText.text( 'Select an Enemy to fight!' ).fadeIn( 350 );
+      } )
     }
   };
 
   winGame = () => {
     console.log( 'YOU WIN' )
+
+    // Animations
+    resetButton.show();
+    attackButton.hide();
+    enemiesText.fadeOut( 150, function() {
+      enemiesText.text( 'You Win!!!' ).fadeIn( 150 );
+    } )
   }
 
   loseGame = () => {
     console.log( 'You lose' );
+
+    resetButton.show();
+    attackButton.hide();
+    enemiesText.fadeOut( 150, function() {
+      enemiesText.text( 'You lose, try again.' ).fadeIn( 150 );
+    } )
   }
+
   // Reset
+  resetButton.on( 'click', function() {
+    initialize();
+  } );
+
 
   // Console log on ` backquote `
   $( document ).keyup( function( event ) {
@@ -180,13 +214,15 @@ $( document ).ready( function() {
       console.log( '======== Begin Log ' + consoleLog + ' ========' )
       if (playerChar) { 
         console.log( 'Player: ' + playerChar.name + ' (' + playerId + ')');
-        console.log( 'Enemies: ' + enemyList );
+        console.log( 'Player Attack: ' + playerChar.atk + " | HP: " + playerChar.hp )
       };
       if (fighterChar) { 
         console.log( 'Fighter: ' + fighterChar.name + ' (' + fighterId + ')' );
+        console.log( 'Fighter Attack: ' + fighterChar.atk + " | HP: " + fighterChar.hp )
       };
+      if ( playerChar ) { console.log( 'Enemies: ' + enemyList ) };
       console.log( '======== End Log ' + consoleLog + ' ========' )
     }
-  });
+  } );
 
-});
+} );
